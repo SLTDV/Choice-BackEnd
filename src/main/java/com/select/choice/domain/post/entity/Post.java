fc7eab37dc2bf928d18a395d3401c91158520945
ssyy0122 <z01039118740@gmail.com>
2022-12-19 15:16:58 +0900

    :sparkles: Post entity 추가
    add

--- src/main/java/com/select/choice/domain/post/entity/Post.java
+++ src/main/java/com/select/choice/domain/post/entity/Post.java
@@ -2,7 +2,27 @@ package com.select.choice.domain.post.entity;
 
 
 import com.select.choice.global.common.entity.BaseIdEntity;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.security.core.userdetails.User;
 
+import javax.persistence.*;
+
+@Entity
 public class Post extends BaseIdEntity {
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private String thumbnail;
+    @Column(nullable = false)
+    private String votingOption1;
+    @Column(nullable = false)
+    private String votingOption2;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private user:User
+
 
 }
