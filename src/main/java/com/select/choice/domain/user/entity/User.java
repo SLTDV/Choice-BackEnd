807952c51edf40314ddd0875b41da26901919559
ssyy0122 <z01039118740@gmail.com>
2022-12-19 20:16:02 +0900

    :sparkles: Comment 생성
    생성

--- src/main/java/com/select/choice/domain/user/entity/User.java
+++ src/main/java/com/select/choice/domain/user/entity/User.java
@@ -1,7 +1,31 @@
 package com.select.choice.domain.user.entity;
 
+import com.select.choice.global.common.entity.BaseIdEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
-public class User {
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseIdEntity {
+    @Column(nullable = false)
+    private String id;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+
+    @Builder
+    public User(String id,String password, String name) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+    }
+
 }
