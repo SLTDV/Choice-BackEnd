c4d86a87e5b8cd75760fd2d9053c9d2f0f2fb87f
shgur7236 <shgurtns7236@naver.com>
2022-12-19 23:39:29 +0900

    :sparkles: AuthDetailsService 구현

---
+++ src/main/java/com/select/choice/global/security/authentication/AuthDetailsService.java
@@ -0,0 +1,21 @@
+package com.select.choice.global.security.authentication;
+
+import com.select.choice.domain.user.exception.UserNotFoundException;
+import com.select.choice.domain.user.repository.UserRepository;
+import com.select.choice.global.error.type.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserById(username)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
+    }
+}
