4e760d5d304f0e6201334021ab5842dc71d7bdaa
shgur7236 <shgurtns7236@naver.com>
2022-12-20 15:58:37 +0900

    :sparkles: SignIn Service

---
+++ src/main/java/com/select/choice/domain/auth/service/Impl/SignInServiceImpl.java
@@ -0,0 +1,34 @@
+package com.select.choice.domain.auth.service.Impl;
+
+import com.select.choice.domain.auth.presentation.request.SignInRequest;
+import com.select.choice.domain.auth.presentation.response.SignInResponse;
+import com.select.choice.domain.auth.service.SignInService;
+import com.select.choice.domain.user.entity.User;
+import com.select.choice.domain.user.facade.UserFacade;
+import com.select.choice.global.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SignInServiceImpl implements SignInService {
+    private final UserFacade userFacade;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public SignInResponse signIn(SignInRequest signInRequest) {
+        User user = userFacade.findUserByEmail(signInRequest.getEmail());
+        userFacade.checkPassword(user, signInRequest.getPassword());
+
+        String accessToken = jwtTokenProvider.generateAccessToken(signInRequest.getEmail());
+        String refreshToken = jwtTokenProvider.generateRefreshToken(signInRequest.getEmail());
+
+        user.updateRefreshToken(refreshToken);
+
+        return SignInResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .expiredAt(jwtTokenProvider.getExpiredTime())
+                .build();
+    }
+}
