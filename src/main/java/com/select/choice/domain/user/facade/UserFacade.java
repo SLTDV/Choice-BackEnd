ca846c3efce176a793fc87c9cf1ab2f9d9491757
shgur7236 <shgurtns7236@naver.com>
2022-12-20 16:02:24 +0900

    :sparkles: UserFacade 생성

---
+++ src/main/java/com/select/choice/domain/user/facade/UserFacade.java
@@ -0,0 +1,30 @@
+package com.select.choice.domain.user.facade;
+
+import com.select.choice.domain.user.entity.User;
+import com.select.choice.domain.user.exception.PasswordNotMatchException;
+import com.select.choice.domain.user.exception.UserNotFoundException;
+import com.select.choice.domain.user.repository.UserRepository;
+import com.select.choice.global.error.type.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
+public class UserFacade {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void checkPassword(User user, String password) {
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new PasswordNotMatchException(ErrorCode.PASSWORD_NOT_MATCH);
+        }
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email).orElseThrow(()
+                -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
+    }
+}
