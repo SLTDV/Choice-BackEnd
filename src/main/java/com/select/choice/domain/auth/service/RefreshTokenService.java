0a18b44d7399fe4fb56554af237883d429417294
shgur7236 <shgurtns7236@naver.com>
2022-12-22 10:32:01 +0900

    :truck: authService 하나에 다 넣기로 변경

--- src/main/java/com/select/choice/domain/auth/service/RefreshTokenService.java
+++
@@ -1,7 +0,0 @@
-package com.select.choice.domain.auth.service;
-
-import com.select.choice.domain.auth.presentation.dto.response.RefreshTokenResponse;
-
-public interface RefreshTokenService {
-    RefreshTokenResponse refresh(String refreshToken);
-}
