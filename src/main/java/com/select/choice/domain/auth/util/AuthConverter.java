8b7b8894546b337bb87a0e57887906ae07412412
shgur7236 <shgurtns7236@naver.com>
2022-12-22 12:06:38 +0900

    :recycle: converter에는 변환만.

--- src/main/java/com/select/choice/domain/auth/util/AuthConverter.java
+++ src/main/java/com/select/choice/domain/auth/util/AuthConverter.java
@@ -1,12 +1,9 @@
 package com.select.choice.domain.auth.util;
 
 import com.select.choice.domain.auth.data.dto.TokenDto;
-import com.select.choice.domain.auth.data.request.SignInRequest;
-import com.select.choice.domain.auth.data.request.SignUpRequest;
 import com.select.choice.domain.auth.data.response.TokenResponse;
 
 public interface AuthConverter {
     TokenResponse toResponse(TokenDto dto);
-    TokenDto toTokenDto(SignInRequest request);
-    TokenDto toTokenDto(SignUpRequest request);
+    TokenDto toTokenDto(String accessToken, String refreshToken, Long expiredAt);
 }
--- src/main/java/com/select/choice/domain/auth/util/AuthConverterImpl.java
+++ src/main/java/com/select/choice/domain/auth/util/AuthConverterImpl.java
@@ -1,12 +1,7 @@
 package com.select.choice.domain.auth.util;
 
 import com.select.choice.domain.auth.data.dto.TokenDto;
-import com.select.choice.domain.auth.data.request.SignInRequest;
-import com.select.choice.domain.auth.data.request.SignUpRequest;
 import com.select.choice.domain.auth.data.response.TokenResponse;
-import com.select.choice.domain.user.entity.User;
-import com.select.choice.domain.user.facade.UserFacade;
-import com.select.choice.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @RequiredArgsConstructor
 public class AuthConverterImpl implements AuthConverter{
-    private final UserFacade userFacade;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public TokenResponse toResponse(TokenDto dto) {
@@ -28,21 +21,7 @@ public class AuthConverterImpl implements AuthConverter{
 
     @Transactional
     @Override
-    public TokenDto toTokenDto(SignInRequest request) {
-        User user = userFacade.findUserByEmail(request.getEmail());
-        userFacade.checkPassword(user, request.getPassword());
-
-        String accessToken = jwtTokenProvider.generateAccessToken(user.getEmail());
-        String refreshToken = jwtTokenProvider.generateRefreshToken(user.getEmail());
-        Long expiredAt = jwtTokenProvider.getExpiredTime(accessToken);
-
-        user.updateRefreshToken(refreshToken);
-
+    public TokenDto toTokenDto(String accessToken, String refreshToken, Long expiredAt) {
         return new TokenDto(accessToken,refreshToken,expiredAt);
-    }
-
-    @Override
-    public TokenDto toTokenDto(SignUpRequest request) {
-        return null;
     }
 }
