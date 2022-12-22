9f66b85b3f2a4dda8f03be9329ab144f2e0e7543
shgur7236 <shgurtns7236@naver.com>
2022-12-22 12:19:18 +0900

    :recycle: 정렬

--- src/main/java/com/select/choice/global/error/type/ErrorCode.java
+++ src/main/java/com/select/choice/global/error/type/ErrorCode.java
@@ -6,20 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
-    // TOKEN
+    PASSWORD_NOT_MATCH("비밀번호가 옳지 않습니다.", 400),
+    CHECK_PASSWORD_NOT_MATCH("비밀번호를 다시 확인해주세요", 400),
     INVALID_TOKEN("유효하지 않은 토큰입니다.", 403),
     EXPIRED_TOKEN("만료된 토큰 입니다.", 403),
-
-    // AUTH
-    PASSWORD_NOT_MATCH("비밀번호가 옳지 않습니다.", 400),
-    DUPLICATE_EMAIL("이미 존재하는 이메일 입니다.", 409),
-    CHECK_PASSWORD_NOT_MATCH("비밀번호를 다시 확인해주세요", 400),
-
-    // USER
     USER_NOT_FOUND("존재하지 않은 USER 입니다.", 404),
+    DUPLICATE_EMAIL("이미 존재하는 이메일 입니다.", 409),
     DUPLICATE_NICKNAME("이미 존재하는 닉네임 입니다.", 409);
-
 
     private final String message;
     private final int status;
