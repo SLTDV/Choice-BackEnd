7f321d4e32a0e7e6f96072525cd6f5b84e165353
shgur7236 <shgurtns7236@naver.com>
2022-12-22 17:07:50 +0900

    :sparkles: UNAUTHORIZED 생성

--- src/main/java/com/select/choice/global/error/type/ErrorCode.java
+++ src/main/java/com/select/choice/global/error/type/ErrorCode.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     PASSWORD_NOT_MATCH("비밀번호가 옳지 않습니다.", 400),
+    UNAUTHORIZED("UNAUTHORIZED", 401),
     INVALID_TOKEN("유효하지 않은 토큰입니다.", 403),
     EXPIRED_TOKEN("만료된 토큰 입니다.", 403),
     USER_NOT_FOUND("존재하지 않은 USER 입니다.", 404),
