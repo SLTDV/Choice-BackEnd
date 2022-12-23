010fd2d172f7a9be6020c9977e932fb5ca7662ad
shgur7236 <shgurtns7236@naver.com>
2022-12-23 15:16:43 +0900

    :sparkles: sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,4 @@
 INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count)
-values (1,'제목','민도현','https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','탕수육','김도현',120,130);
-
-
-
-
+values (1,'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','제목','민도현','탕수육','김도현',30,130);
+INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count)
+values (3,'https://thumbnews.nateimg.co.kr/view610///news.nateimg.co.kr/orgImg/st/2022/10/05/05_830172_17873.jpg','강해림이쁘다 ㅇㅈ?','ㅇㄴㅁㄴㅇㅁㄴㅇㅁㄴㅇㄴㅁㅇㄴ','탕수육','정명직',90,20);
