4102408e3dcd479111c6cf34bcffbed628483b9f
ssyy0122 <z01039118740@gmail.com>
2022-12-23 10:20:40 +0900

    :sparkles: Dtd 값 넣기
    d

--- src/main/java/com/select/choice/domain/post/domain/request/CreatePostRequestDto.java
+++ src/main/java/com/select/choice/domain/post/domain/request/CreatePostRequestDto.java
@@ -1,5 +1,18 @@
 package com.select.choice.domain.post.domain.request;
 
-public class CreatePostRequestDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+public class CreatePostRequestDto {
+    private String title;
+    private String content;
+    private String thumbnail;
+    private String firstVotingOption;
+    private String secondVotingOtion;
 }
