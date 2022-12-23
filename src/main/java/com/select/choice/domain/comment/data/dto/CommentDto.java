2f2629f49729705a34d180d64d22eb7246072c5e
shgur7236 <shgurtns7236@naver.com>
2022-12-23 17:45:12 +0900

    :sparkles: CommentDto 생성

---
+++ src/main/java/com/select/choice/domain/comment/data/dto/CommentDto.java
@@ -0,0 +1,14 @@
+package com.select.choice.domain.comment.data.dto;
+
+import com.select.choice.domain.post.domain.data.entity.Post;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class CommentDto {
+    private final Post post;
+    private final String content;
+}
