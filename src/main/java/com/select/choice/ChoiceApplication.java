05d36141c68ea4a107b6a5dee2de596e509adce9
ssyy0122 <z01039118740@gmail.com>
2022-12-28 00:29:24 +0900

    :sparkles @EnableJpaproperties를 추가해서 실행

--- src/main/java/com/select/choice/ChoiceApplication.java
+++ src/main/java/com/select/choice/ChoiceApplication.java
@@ -3,9 +3,11 @@ package com.select.choice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ConfigurationPropertiesScan(basePackages = {"com.select.*"})
 @SpringBootApplication
+@EnableJpaRepositories
 public class ChoiceApplication {
 
 	public static void main(String[] args) {
