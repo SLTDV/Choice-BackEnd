afcec80aa854cae4af0d7d0f2eff49b246e982ed
shgur7236 <shgurtns7236@naver.com>
2022-12-27 16:18:48 +0900

    :sparkles: 닉네임 변경 서비스 구현

--- src/main/java/com/select/choice/domain/user/domain/Service/Impl/UserServiceImpl.java
+++ src/main/java/com/select/choice/domain/user/domain/Service/Impl/UserServiceImpl.java
@@ -4,12 +4,14 @@ import com.select.choice.domain.post.domain.data.entity.Post;
 import com.select.choice.domain.post.domain.repository.PostRepository;
 import com.select.choice.domain.user.domain.Service.UserService;
 import com.select.choice.domain.user.domain.data.dto.MyPageDto;
+import com.select.choice.domain.user.domain.data.dto.NicknameDto;
 import com.select.choice.domain.user.domain.data.entity.User;
 import com.select.choice.domain.user.domain.data.response.GetMyPageResponse;
 import com.select.choice.domain.user.domain.facade.UserFacade;
 import com.select.choice.domain.user.domain.util.UserConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -34,5 +36,12 @@ public class UserServiceImpl implements UserService {
         MyPageDto myPageDto = userConverter.toMyPageDto(user, postList);
 
         return userConverter.toMyPageResponse(myPageDto);
+    }
+
+    @Transactional
+    @Override
+    public void changeNickname(NicknameDto nicknameDto) {
+        User user = userFacade.currentUser();
+        user.updateNickname(nicknameDto.getNickname());
     }
 }
--- src/main/java/com/select/choice/domain/user/domain/Service/UserService.java
+++ src/main/java/com/select/choice/domain/user/domain/Service/UserService.java
@@ -1,8 +1,11 @@
 package com.select.choice.domain.user.domain.Service;
 
+import com.select.choice.domain.user.domain.data.dto.NicknameDto;
 import com.select.choice.domain.user.domain.data.response.GetMyPageResponse;
 
 public interface UserService {
     void delete();
     GetMyPageResponse getMyPage();
+
+    void changeNickname(NicknameDto nicknameDto);
 }
