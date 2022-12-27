676fe730ba826e5c74eaad58d0a2b56296e555e0
ssyy0122 <z01039118740@gmail.com>
2022-12-28 00:30:52 +0900

    :sparkles Dto 생성

---
+++ src/main/java/com/select/choice/domain/post/domain/data/dto/PostDetailDto.java
@@ -0,0 +1,24 @@
+
+package com.select.choice.domain.post.domain.data.dto;
+
+import com.select.choice.domain.comment.data.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class PostDetailDto {
+    private String authorname;
+    private String thumbnail;
+    private String title;
+    private String content;
+    private String firstVotingOption;
+    private String secondVotingOption;
+    private Integer firstVotingCount;
+    private Integer secondVotingCount;
+    List<Comment> comment;
+}
