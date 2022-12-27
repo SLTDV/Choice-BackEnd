93020b43131854e2434c603b4b954c0b13a980f7
ssyy0122 <z01039118740@gmail.com>
2022-12-28 00:32:15 +0900

    :sparkles service 구현

--- src/main/java/com/select/choice/domain/post/domain/service/impl/PostServiceImpl.java
+++ src/main/java/com/select/choice/domain/post/domain/service/impl/PostServiceImpl.java
@@ -1,14 +1,19 @@
 package com.select.choice.domain.post.domain.service.impl;
 
+import com.select.choice.domain.comment.data.entity.Comment;
+import com.select.choice.domain.comment.repository.CommentRepository;
 import com.select.choice.domain.post.domain.data.dto.CreatePostDto;
+import com.select.choice.domain.post.domain.data.dto.PostDetailDto;
 import com.select.choice.domain.post.domain.data.dto.PostDto;
 import com.select.choice.domain.post.domain.data.entity.Post;
+import com.select.choice.domain.post.domain.data.response.PostDetailResponse;
 import com.select.choice.domain.post.domain.exception.PostNotFoundException;
 import com.select.choice.domain.post.domain.repository.PostRepository;
 import com.select.choice.domain.post.domain.service.PostService;
 import com.select.choice.domain.post.domain.util.PostConverter;
 import com.select.choice.domain.user.data.entity.User;
 import com.select.choice.domain.user.facade.UserFacade;
+import com.select.choice.domain.user.repository.UserRepository;
 import com.select.choice.global.error.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,6 +27,8 @@ public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
     private final PostConverter postConverter;
     private final UserFacade userFacade;
+    private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
 
     @Override
     public List<PostDto> getAllPostList() {
@@ -41,6 +48,15 @@ public class PostServiceImpl implements PostService {
         User user = userFacade.currentUser();
         Post post = postConverter.toEntity(postDto, user);
         postRepository.save(post);
+    }
+
+    @Override
+    public PostDetailResponse aggregateDetail(Long postIdx) {
+        User user = userFacade.currentUser();
+        Post post = postRepository.findByIdx(postIdx);
+        List<Comment> comment = commentRepository.findAllByPostIdx(postIdx);
+        PostDetailDto postDetailDto = postConverter.postDetailDto(post,comment,user);
+        return postConverter.toDetailResponse(postDetailDto);
     }
 
     @Override
