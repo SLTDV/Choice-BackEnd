6f1adac3b8c84c22d3c49ab8dd33dac2b442090a
ssyy0122 <z01039118740@gmail.com>
2022-12-28 00:31:04 +0900

    :sparkles Repo생성

---
+++ src/main/java/com/select/choice/domain/post/domain/data/response/PostDetailResponse.java
@@ -0,0 +1,24 @@
+package com.select.choice.domain.post.domain.data.response;
+
+import com.select.choice.domain.comment.data.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class PostDetailResponse {
+    private String authorname;
+    private String thumbnail;
+    private String title;
+    private String content;
+    private String firstVotingOption;
+    private String secondVotingOption;
+    private Integer firstVotingCount;
+    private Integer secondVotingCount;
+    List<Comment> comment;
+}
