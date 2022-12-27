693d0a93a84d0aa9e6effb782ba73e9604fec804
ssyy0122 <z01039118740@gmail.com>
2022-12-27 20:37:57 +0900

    :sparkles 컨버터에 메서드 지정

--- src/main/java/com/select/choice/domain/post/domain/util/PostConverter.java
+++ src/main/java/com/select/choice/domain/post/domain/util/PostConverter.java
@@ -2,9 +2,11 @@ package com.select.choice.domain.post.domain.util;
 
 import com.select.choice.domain.post.domain.data.dto.AddCountDto;
 import com.select.choice.domain.post.domain.data.dto.CreatePostDto;
+import com.select.choice.domain.post.domain.data.dto.PostDetailDto;
 import com.select.choice.domain.post.domain.data.dto.PostDto;
 import com.select.choice.domain.post.domain.data.entity.Post;
 import com.select.choice.domain.post.domain.data.request.AddCountRequest;
+import com.select.choice.domain.post.domain.data.response.PostDetailResponse;
 import com.select.choice.domain.post.domain.data.response.PostResponse;
 import com.select.choice.domain.post.domain.data.request.CreatePostRequestDto;
 import com.select.choice.domain.user.domain.data.entity.User;
@@ -16,7 +18,7 @@ public interface PostConverter {
     List<PostDto>toPostDto(List<Post>post);
     List<PostDto>toBestPostDto(List<Post>post);
     Post toEntity(CreatePostDto dto, User user);
-
+    PostDetailResponse toDetailResponse(PostDetailDto postDetailDto);
     CreatePostDto toCreatePost(CreatePostRequestDto createPostRequestDto);
 
     AddCountDto toAddCountDto(AddCountRequest addCountRequest);
