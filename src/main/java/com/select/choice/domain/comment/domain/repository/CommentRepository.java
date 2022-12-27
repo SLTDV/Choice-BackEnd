ceb40ee2b540914407a8da0280bccb6d28cacc9d
ssyy0122 <z01039118740@gmail.com>
2022-12-27 20:44:32 +0900

    :sparkles List type으로 변경

--- src/main/java/com/select/choice/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/select/choice/domain/comment/domain/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.select.choice.domain.comment.domain.repository;
 import com.select.choice.domain.comment.domain.data.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+   List<Comment> findAll(Long postIdx);
 }
