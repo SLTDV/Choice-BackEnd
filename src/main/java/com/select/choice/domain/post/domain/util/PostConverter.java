87c54be90474445ee5045619ee384bfef1efb2bf
ssyy0122 <z01039118740@gmail.com>
2022-12-27 20:37:57 +0900

    :sparkles 컨버터에 메서드 지정

--- src/main/java/com/select/choice/domain/post/domain/util/PostConverter.java
+++ src/main/java/com/select/choice/domain/post/domain/util/PostConverter.java
@@ -1,8 +1,10 @@
 package com.select.choice.domain.post.domain.util;
 
 import com.select.choice.domain.post.domain.data.dto.CreatePostDto;
+import com.select.choice.domain.post.domain.data.dto.PostDetailDto;
 import com.select.choice.domain.post.domain.data.dto.PostDto;
 import com.select.choice.domain.post.domain.data.entity.Post;
+import com.select.choice.domain.post.domain.data.response.PostDetailResponse;
 import com.select.choice.domain.post.domain.data.response.PostResponse;
 import com.select.choice.domain.post.domain.data.request.CreatePostRequestDto;
 import com.select.choice.domain.user.data.entity.User;
@@ -13,6 +15,6 @@ public interface PostConverter {
     List<PostResponse> toResponse(List<PostDto> dto);
     List<PostDto>toPostDto(List<Post>post);
     Post toEntity(CreatePostDto dto, User user);
-
+    PostDetailResponse toDetailResponse(PostDetailDto postDetailDto);
     CreatePostDto toCreatePost(CreatePostRequestDto createPostRequestDto);
 }
