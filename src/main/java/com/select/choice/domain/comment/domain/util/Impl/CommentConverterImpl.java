cba8f955f3cdf95b6411c4f1db7f57736279fbb9
shgur7236 <shgurtns7236@naver.com>
2022-12-30 14:42:16 +0900

    :bug: List<Comment> -> List<CommentDetailDto> 구현

--- src/main/java/com/select/choice/domain/comment/domain/util/Impl/CommentConverterImpl.java
+++ src/main/java/com/select/choice/domain/comment/domain/util/Impl/CommentConverterImpl.java
@@ -1,5 +1,6 @@
 package com.select.choice.domain.comment.domain.util.Impl;
 
+import com.select.choice.domain.comment.domain.data.dto.CommentDetailDto;
 import com.select.choice.domain.comment.domain.data.dto.CommentDto;
 import com.select.choice.domain.comment.domain.data.entity.Comment;
 import com.select.choice.domain.comment.domain.util.CommentConverter;
@@ -7,6 +8,10 @@ import com.select.choice.domain.post.domain.data.entity.Post;
 import com.select.choice.domain.user.domain.data.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -27,5 +32,16 @@ public class CommentConverterImpl implements CommentConverter {
         return CommentDto.builder()
                 .content(content)
                 .build();
+    }
+
+    @Override
+    public List<CommentDetailDto> toDetailDto(List<Comment> comment) {
+        return comment.stream().map(list ->
+                new CommentDetailDto(
+                        list.getIdx(),
+                        list.getContent(),
+                        list.getUser().getNickname()
+                )
+        ).sorted(Comparator.comparing(CommentDetailDto::getIdx).reversed()).collect(Collectors.toList());
     }
 }
