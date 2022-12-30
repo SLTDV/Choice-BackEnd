5a45fba0786f6b9c0ad1bdb80660c74ee1799608
shgur7236 <shgurtns7236@naver.com>
2022-12-30 15:01:55 +0900

    :sparkles: is_voting 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO user(idx, email, nickname, password) VALUES (1,'admin1234@naver.com','admin','$2a$10$Tt/456M.srVrbd6uoFuDo.ShkoXutSpVJd4lGaVqJHZvaaFpc4SaO');
 INSERT INTO user(idx, email, nickname, password) VALUES (2,'user1234@naver.com','user','$2a$10$Tt/456M.srVrbd6uoFuDo.ShkoXutSpVJd4lGaVqJHZvaaFpc4SaO');
 
-INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count,user_idx) values (1, 'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','1','민도현','탕수육','김도현',0,10,1);
-INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count,user_idx) values (2, 'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','2','민도현','탕수육','김도현',10,10,1);
-INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count,user_idx) values (3, 'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','3','민도현','탕수육','김도현',20,10,1);
-INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count,user_idx) values (4, 'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','4','민도현','탕수육','김도현',30,10,1);
+INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count,user_idx,is_voting) values (1, 'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','1','민도현','탕수육','김도현',0,10,1,false);
+INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count,user_idx,is_voting) values (2, 'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','2','민도현','탕수육','김도현',10,10,1,false);
+INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count,user_idx,is_voting) values (3, 'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','3','민도현','탕수육','김도현',20,10,1,false);
+INSERT INTO post(idx, thumbnail, title, content, first_voting_option, second_voting_option, first_voting_count, second_voting_count,user_idx,is_voting) values (4, 'https://cdn.crowdpic.net/list-thumb/thumb_l_4291713E6EC8D22461618B2107D30880.jpg','4','민도현','탕수육','김도현',30,10,1,false);
 
