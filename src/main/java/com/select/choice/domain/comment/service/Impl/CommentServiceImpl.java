85c2c1e2429ef16d4072af4dc74dd42a30294b77
shgur7236 <shgurtns7236@naver.com>
2023-03-08 18:25:31 +0900

    :sparkles: util 추가

--- src/main/java/com/select/choice/domain/comment/service/Impl/CommentServiceImpl.java
+++ src/main/java/com/select/choice/domain/comment/service/Impl/CommentServiceImpl.java
@@ -2,11 +2,11 @@ package com.select.choice.domain.comment.service.Impl;
 
 import com.select.choice.domain.comment.data.dto.CommentDto;
 import com.select.choice.domain.comment.data.entity.Comment;
-import com.select.choice.domain.comment.exception.CommentNotFoundException;
 import com.select.choice.domain.comment.exception.IsNotMyCommentException;
 import com.select.choice.domain.comment.repository.CommentRepository;
 import com.select.choice.domain.comment.service.CommentService;
 import com.select.choice.domain.comment.util.CommentConverter;
+import com.select.choice.domain.comment.util.CommentUtil;
 import com.select.choice.domain.post.data.entity.Post;
 import com.select.choice.domain.post.exception.PostNotFoundException;
 import com.select.choice.domain.post.repository.PostRepository;
@@ -24,12 +24,14 @@ public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final CommentConverter commentConverter;
     private final PostRepository postRepository;
+    private final CommentUtil commentUtil;
 
     @Transactional
     @Override
     public void write(Long postIdx,CommentDto commentDto) {
         User user = userFacade.currentUser();
-        Post post = postRepository.findById(postIdx).orElseThrow(() -> new PostNotFoundException(ErrorCode.POST_NOT_FOUND));
+        Post post = postRepository.findById(postIdx)
+                .orElseThrow(() -> new PostNotFoundException(ErrorCode.POST_NOT_FOUND));
         Comment comment = commentConverter.toEntity(commentDto, user, post);
         commentRepository.save(comment);
     }
@@ -37,7 +39,7 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public void edit(Long commentIdx, CommentDto commentDto) {
         User user = userFacade.currentUser();
-        Comment comment = commentRepository.findById(commentIdx).orElseThrow(() -> new CommentNotFoundException(ErrorCode.Comment_NOT_FOUND));
+        Comment comment = commentUtil.findById(commentIdx);
         if(!comment.getUser().equals(user)){
             throw new IsNotMyCommentException(ErrorCode.IS_NOT_MY_COMMENT);
         }
@@ -47,7 +49,7 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public void delete(Long commentIdx) {
-        Comment comment = commentRepository.findById(commentIdx).orElseThrow(() -> new CommentNotFoundException(ErrorCode.Comment_NOT_FOUND));
+        Comment comment = commentUtil.findById(commentIdx);
         commentRepository.delete(comment);
     }
 }
