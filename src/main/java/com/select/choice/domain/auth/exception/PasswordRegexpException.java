d3b7a6857f0b6331bfbef6778d3ab11ac07cb252
shgur7236 <shgurtns7236@naver.com>
2023-03-08 17:44:30 +0900

    :recycle: 예외처리 로직 수정

--- src/main/java/com/select/choice/domain/auth/exception/PasswordRegexpException.java
+++ src/main/java/com/select/choice/domain/auth/exception/PasswordRegexpException.java
@@ -1,11 +1,10 @@
 package com.select.choice.domain.auth.exception;
 
+import com.select.choice.global.error.exception.GlobalExceptions;
 import com.select.choice.global.error.type.ErrorCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-@Getter
-public class PasswordRegexpException extends RuntimeException {
-    private final ErrorCode errorCode;
+public class PasswordRegexpException extends GlobalExceptions {
+    public PasswordRegexpException(ErrorCode errorCode) {
+        super(errorCode);
+    }
 }
