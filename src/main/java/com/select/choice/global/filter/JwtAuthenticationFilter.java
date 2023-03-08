05ddc90f830d02b9eb811db9e4e0a39b43cf4724
shgur7236 <shgurtns7236@naver.com>
2023-03-08 17:44:58 +0900

    :recycle: refactoring

--- src/main/java/com/select/choice/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/select/choice/global/filter/JwtAuthenticationFilter.java
@@ -1,12 +1,9 @@
 package com.select.choice.global.filter;
 
 import com.select.choice.global.security.JwtTokenProvider;
-import com.select.choice.global.security.authentication.AuthDetailsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,28 +17,16 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
-    private final AuthDetailsService authDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String accessToken = jwtTokenProvider.resolveToken(request);
 
         if (accessToken != null) {
-            String id = jwtTokenProvider.extractAllClaims(accessToken).getSubject();
-            registerUserInfoSecurityContext(id, request);
+            Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         filterChain.doFilter(request, response);
-    }
-
-    private void registerUserInfoSecurityContext(String email, HttpServletRequest request){
-        try {
-            UserDetails userDetails = authDetailsService.loadUserByUsername(email);
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-        } catch (NullPointerException e) {
-            throw new RuntimeException();
-        }
     }
 }
 
