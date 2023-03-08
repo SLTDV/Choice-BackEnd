2f197b2cf2096ab34358806844a529e1b4f11d19
shgur7236 <shgurtns7236@naver.com>
2023-03-08 17:51:13 +0900

    :recycle: dto 값 수정

--- src/main/java/com/select/choice/domain/auth/presentation/data/response/TokenResponse.java
+++ src/main/java/com/select/choice/domain/auth/presentation/data/response/TokenResponse.java
@@ -2,11 +2,13 @@ package com.select.choice.domain.auth.presentation.data.response;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @RequiredArgsConstructor
 public class TokenResponse {
     private final String accessToken;
     private final String refreshToken;
-    private final Long expiredAt;
-}
+    private final LocalDateTime accessExpiredTime;
+    private final LocalDateTime refreshExpiredTime;}
