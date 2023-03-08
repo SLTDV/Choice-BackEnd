eabe8f4d17d9b167a8280be2acf645645c95047b
shgur7236 <shgurtns7236@naver.com>
2023-03-08 20:44:15 +0900

    :recycle: dto 수정

--- src/main/java/com/select/choice/domain/image/presentation/data/dto/ImageUploadDto.java
+++ src/main/java/com/select/choice/domain/image/presentation/data/dto/ImageUploadDto.java
@@ -4,9 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 @Builder
+@RequiredArgsConstructor
 public class ImageUploadDto {
     private final String imageUrl;
 }
--- src/main/java/com/select/choice/domain/image/presentation/data/response/ImageResponse.java
+++ src/main/java/com/select/choice/domain/image/presentation/data/response/ImageResponse.java
@@ -4,9 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 @Builder
+@RequiredArgsConstructor
 public class ImageResponse {
     private final String imageUrl;
 }
