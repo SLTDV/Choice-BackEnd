eaf1c5db3b48e9f7f4db12c475ee32e5ac3500d0
shgur7236 <shgurtns7236@naver.com>
2023-03-09 17:41:05 +0900

    :recycle: dto 변경

--- src/main/java/com/select/choice/domain/post/service/PostService.java
+++ src/main/java/com/select/choice/domain/post/service/PostService.java
@@ -1,23 +1,20 @@
 package com.select.choice.domain.post.service;
 
 
-import com.select.choice.domain.post.presentation.data.dto.AddCountDto;
-import com.select.choice.domain.post.presentation.data.dto.CreatePostDto;
-import com.select.choice.domain.post.presentation.data.dto.AllPostListDto;
+import com.select.choice.domain.post.presentation.data.dto.*;
 import com.select.choice.domain.post.presentation.data.response.AddCountResponse;
 import com.select.choice.domain.post.presentation.data.response.PostDetailResponse;
-import com.select.choice.domain.post.presentation.data.response.AllPostListResponse;
 
 import java.util.List;
 
 public interface PostService {
 
-    List<AllPostListDto> getAllPostList();
-    List<AllPostListResponse> getBestPostList();
+    List<PostDto> getAllPostList();
+    List<PostDto> getBestPostList();
     void createPost(CreatePostDto createPostDto);
-    PostDetailResponse aggregateDetail(Long postIdx);
+    PostDetailDto aggregateDetail(Long postIdx);
 
     void deletePost(Long postIdx);
 
-    AddCountResponse addCount(AddCountDto addCountDto, Long postIdx);
+    VoteCountDto addCount(AddCountDto addCountDto, Long postIdx);
 }
