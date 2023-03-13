224f4c6c46e02e5d4eeb939bb971268f548bcf1d
shgur7236 <shgurtns7236@naver.com>
2023-03-13 10:03:05 +0900

    :recycle: add -> vote

--- src/main/java/com/select/choice/domain/post/service/PostService.java
+++ src/main/java/com/select/choice/domain/post/service/PostService.java
@@ -2,8 +2,6 @@ package com.select.choice.domain.post.service;
 
 
 import com.select.choice.domain.post.presentation.data.dto.*;
-import com.select.choice.domain.post.presentation.data.response.AddCountResponse;
-import com.select.choice.domain.post.presentation.data.response.PostDetailResponse;
 
 import java.util.List;
 
@@ -13,8 +11,6 @@ public interface PostService {
     List<PostDto> getBestPostList();
     void createPost(CreatePostDto createPostDto);
     PostDetailDto aggregateDetail(Long postIdx);
-
     void deletePost(Long postIdx);
-
-    VoteCountDto addCount(AddCountDto addCountDto, Long postIdx);
+    VoteCountDto voteCount(AddCountDto addCountDto, Long postIdx);
 }
