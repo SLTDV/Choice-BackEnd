f352f7461cf1d974d9db500ba4b5a2d77914a2ac
shgur7236 <shgurtns7236@naver.com>
2023-03-15 22:26:47 +0900

    :sparkles: 메서드 생성

--- src/main/java/com/select/choice/domain/post/domain/repository/VotingPostRepository.java
+++ src/main/java/com/select/choice/domain/post/domain/repository/VotingPostRepository.java
@@ -1,8 +1,11 @@
 package com.select.choice.domain.post.domain.repository;
 
+import com.select.choice.domain.post.domain.entity.Post;
 import com.select.choice.domain.post.domain.entity.VotingPost;
+import com.select.choice.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VotingPostRepository extends JpaRepository<VotingPost, Long> {
-    boolean existsByUserAndPost();
+    boolean existsByUserAndPost(User user, Post post);
+    VotingPost findByUserAndPost(User user, Post post);
 }
