fd5e6f494581d06b6b1e4a8ea8a15abacceaaa53
shgur7236 <shgurtns7236@naver.com>
2023-03-16 14:42:39 +0900

    :sparkles: Controller 구현

--- src/main/java/com/select/choice/domain/user/presentation/UserController.java
+++ src/main/java/com/select/choice/domain/user/presentation/UserController.java
@@ -1,9 +1,11 @@
 package com.select.choice.domain.user.presentation;
 
 import com.select.choice.domain.user.Service.UserService;
+import com.select.choice.domain.user.presentation.data.dto.ChangeProfileImageDto;
 import com.select.choice.domain.user.presentation.data.dto.MyPageDto;
 import com.select.choice.domain.user.presentation.data.dto.NicknameDto;
 import com.select.choice.domain.user.presentation.data.request.ChangeNicknameRequest;
+import com.select.choice.domain.user.presentation.data.request.ChangeProfileImageRequest;
 import com.select.choice.domain.user.presentation.data.response.GetMyPageResponse;
 import com.select.choice.domain.user.util.UserConverter;
 import lombok.RequiredArgsConstructor;
@@ -47,6 +49,17 @@ public class UserController {
     public ResponseEntity<Void> changeNickname(@RequestBody ChangeNicknameRequest changeNicknameRequest){
         NicknameDto nicknameDto = userConverter.toDto(changeNicknameRequest);
         userService.changeNickname(nicknameDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    /*
+    기능: 프로필 이미지 변경
+    담당자: 노혁
+     */
+    @PatchMapping("/image")
+    public ResponseEntity<Void> changeProfileImage(@RequestBody ChangeProfileImageRequest changeProfileImageRequest){
+        ChangeProfileImageDto changeProfileImageDto = userConverter.toDto(changeProfileImageRequest);
+        userService.changeProfileImage(changeProfileImageDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
