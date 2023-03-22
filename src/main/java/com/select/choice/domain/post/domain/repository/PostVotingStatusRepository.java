a562d178cab50a33ab06f83c104a0e4542e86cb6
shgur7236 <shgurtns7236@naver.com>
2023-03-22 10:20:03 +0900

    :bug: fix

--- src/main/java/com/select/choice/domain/post/domain/repository/PostVotingStatusRepository.java
+++ src/main/java/com/select/choice/domain/post/domain/repository/PostVotingStatusRepository.java
@@ -4,7 +4,9 @@ import com.select.choice.domain.post.domain.entity.Post;
 import com.select.choice.domain.post.domain.entity.PostVotingStatus;
 import com.select.choice.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostVotingStatusRepository extends JpaRepository<PostVotingStatus, Long> {
     PostVotingStatus findByUserAndPost(User user, Post post);
 }
