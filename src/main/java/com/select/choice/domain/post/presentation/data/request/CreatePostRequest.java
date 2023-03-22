c4e5f76f1fac3cdc58749fe45a2b4412c51865c3
shgur7236 <shgurtns7236@naver.com>
2023-03-22 23:30:57 +0900

    :sparkles: @Size값 추가

--- src/main/java/com/select/choice/domain/post/presentation/data/request/CreatePostRequest.java
+++ src/main/java/com/select/choice/domain/post/presentation/data/request/CreatePostRequest.java
@@ -2,10 +2,14 @@ package com.select.choice.domain.post.presentation.data.request;
 
 import lombok.*;
 
+import javax.validation.constraints.Size;
+
 @Getter
 @RequiredArgsConstructor
 public class CreatePostRequest {
+    @Size(min = 2, max = 16)
     private final String title;
+    @Size(min = 4, max = 100)
     private final String content;
     private final String firstVotingOption;
     private final String secondVotingOption;
