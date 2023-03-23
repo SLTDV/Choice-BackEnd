2f150c1fa23a34e0568dedbc990d76447e64a28f
shgur7236 <shgurtns7236@naver.com>
2023-03-23 10:10:12 +0900

    :sparkles: UserConverter 구현

--- src/main/java/com/select/choice/domain/user/util/UserConverter.java
+++ src/main/java/com/select/choice/domain/user/util/UserConverter.java
@@ -2,12 +2,14 @@ package com.select.choice.domain.user.util;
 
 import com.select.choice.domain.post.domain.entity.Post;
 import com.select.choice.domain.user.presentation.data.dto.ChangeProfileImageDto;
+import com.select.choice.domain.user.presentation.data.dto.HeaderDto;
 import com.select.choice.domain.user.presentation.data.dto.MyPageDto;
 import com.select.choice.domain.user.presentation.data.dto.NicknameDto;
 import com.select.choice.domain.user.domain.entity.User;
 import com.select.choice.domain.user.presentation.data.request.ChangeNicknameRequest;
 import com.select.choice.domain.user.presentation.data.request.ChangeProfileImageRequest;
 import com.select.choice.domain.user.presentation.data.response.GetMyPageResponse;
+import com.select.choice.domain.user.presentation.data.response.HeaderResponse;
 
 import java.util.List;
 
@@ -16,4 +18,6 @@ public interface UserConverter {
     GetMyPageResponse toResponse(MyPageDto myPageDto);
     NicknameDto toDto(ChangeNicknameRequest changeNicknameRequest);
     ChangeProfileImageDto toDto(ChangeProfileImageRequest changeProfileImageRequest);
+    HeaderDto toDto(User user);
+    HeaderResponse toResponse(HeaderDto headerDto);
 }
