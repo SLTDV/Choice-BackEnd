98f92e28ad8541fb5ebcadd1b7a0e0b623664b8f
shgur7236 <shgurtns7236@naver.com>
2023-03-23 09:09:24 +0900

    :sparkles: @Size값 변경

--- src/main/java/com/select/choice/domain/post/presentation/data/request/CreatePostRequest.java
+++ src/main/java/com/select/choice/domain/post/presentation/data/request/CreatePostRequest.java
@@ -9,9 +9,11 @@ import javax.validation.constraints.Size;
 public class CreatePostRequest {
     @Size(min = 2, max = 16)
     private final String title;
-    @Size(min = 4, max = 100)
+    @Size(min = 2, max = 100)
     private final String content;
+    @Size(min = 2, max = 8)
     private final String firstVotingOption;
+    @Size(min = 2, max = 8)
     private final String secondVotingOption;
     private final String firstImageUrl;
     private final String secondImageUrl;
