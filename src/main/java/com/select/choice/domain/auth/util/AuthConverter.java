4ad0bdf827ac6231215c621a809d64635d865e91
shgur7236 <shgurtns7236@naver.com>
2023-03-27 10:17:16 +0900

    :sparkles: converter 구현

--- src/main/java/com/select/choice/domain/auth/util/AuthConverter.java
+++ src/main/java/com/select/choice/domain/auth/util/AuthConverter.java
@@ -1,9 +1,11 @@
 package com.select.choice.domain.auth.util;
 
 import com.select.choice.domain.auth.domain.entity.RefreshToken;
+import com.select.choice.domain.auth.presentation.data.dto.SendPhoneNumberDto;
 import com.select.choice.domain.auth.presentation.data.dto.SignInDto;
 import com.select.choice.domain.auth.presentation.data.dto.SignUpDto;
 import com.select.choice.domain.auth.presentation.data.dto.TokenDto;
+import com.select.choice.domain.auth.presentation.data.request.SendPhoneNumberRequest;
 import com.select.choice.domain.auth.presentation.data.request.SignInRequest;
 import com.select.choice.domain.auth.presentation.data.request.SignUpRequest;
 import com.select.choice.domain.auth.presentation.data.response.TokenResponse;
@@ -14,11 +16,9 @@ import java.time.LocalDateTime;
 public interface AuthConverter {
     TokenResponse toResponse(TokenDto dto);
     TokenDto toDto(String accessToken, String refreshToken, LocalDateTime accessExp, LocalDateTime refreshExp);
-
     SignInDto toDto(SignInRequest signInRequest);
-
     SignUpDto toDto(SignUpRequest signUpRequest);
-
     User toEntity(SignUpDto signUpDto);
     RefreshToken toEntity(Long userIdx, String refreshToken);
+    SendPhoneNumberDto toDto(SendPhoneNumberRequest sendPhoneNumberRequest);
 }
