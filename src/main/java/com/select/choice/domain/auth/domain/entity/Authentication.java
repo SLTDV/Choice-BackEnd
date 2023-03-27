6660cc2071bd518f2541036449046682624a1b2c
shgur7236 <shgurtns7236@naver.com>
2023-03-27 17:13:20 +0900

    :sparkles: Redis Table 구성

---
+++ src/main/java/com/select/choice/domain/auth/domain/entity/Authentication.java
@@ -0,0 +1,23 @@
+package com.select.choice.domain.auth.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "Authentication")
+public class Authentication {
+    @Id
+    @Indexed
+    private String email;
+
+    @Builder
+    public Authentication(String email){
+        this.email = email;
+    }
+}
