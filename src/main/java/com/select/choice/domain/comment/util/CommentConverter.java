b3f308168ae784ad30102159baa821d4886eff86
shgur7236 <shgurtns7236@naver.com>
2023-04-05 15:13:24 +0900

    :sparkles: isMine 반환 값 추가

--- src/main/java/com/select/choice/domain/comment/util/CommentConverter.java
+++ src/main/java/com/select/choice/domain/comment/util/CommentConverter.java
@@ -1,12 +1,11 @@
 package com.select.choice.domain.comment.util;
 
-import com.select.choice.domain.comment.presentation.data.dto.CommentDetailDto;
+import com.select.choice.domain.post.presentation.data.dto.CommentDetailDto;
 import com.select.choice.domain.comment.presentation.data.dto.CommentDto;
 import com.select.choice.domain.comment.domain.entity.Comment;
 import com.select.choice.domain.comment.presentation.data.request.EditCommentRequest;
 import com.select.choice.domain.comment.presentation.data.request.WriteCommentRequest;
 import com.select.choice.domain.post.domain.entity.Post;
-import com.select.choice.domain.post.domain.entity.PostVotingStatus;
 import com.select.choice.domain.user.domain.entity.User;
 
 import java.util.List;
@@ -15,7 +14,5 @@ public interface CommentConverter {
     Comment toEntity(CommentDto commentDto, User user, Post post);
     CommentDto toDto(EditCommentRequest editCommentRequest);
     CommentDto toDto(WriteCommentRequest writeCommentRequest);
-    List<CommentDetailDto> toDto(List<Comment> comment);
-    PostVotingStatus toEntity(int choiceOption, User user, Post post);
-
+    List<CommentDetailDto> toDto(List<Comment> comment, User user);
 }
