7306ea38dcc80ef555e8d606f3ddd3186557f942
shgur7236 <shgurtns7236@naver.com>
2023-04-11 20:23:18 +0900

    :sparkles: dto

---
+++ src/main/java/com/select/choice/domain/post/presentation/data/dto/WebVerPostDetailDto.java
@@ -0,0 +1,20 @@
+package com.select.choice.domain.post.presentation.data.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class WebVerPostDetailDto {
+    private final String title;
+    private final String content;
+    private final String firstImageUrl;
+    private final String secondImageUrl;
+    private final String writer;
+    private final String profileImageUrl;
+    private final List<CommentDetailDto> comment;
+}
