75da80955167239a3b6114aba4612a321b9381aa
shgur7236 <shgurtns7236@naver.com>
2023-04-18 21:07:52 +0900

    :sparkles: pagination repository 부분 구현

--- src/main/java/com/select/choice/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/select/choice/domain/post/domain/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.select.choice.domain.post.domain.repository;
 
 import com.select.choice.domain.post.domain.entity.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
     @Query("select p from Post p order by (p.firstVotingCount + p.secondVotingCount) desc")
-    List<Post> getBestPostList();
+    List<Post> getBestPostList(Pageable pageable);
 
     List<Post> findAllByUserIdx(Long userIdx);
     List<Post> findAllByCreatedAtContaining(String today);
