647f2db72be5897ee577854073661fb60e52cae3
shgur7236 <shgurtns7236@naver.com>
2023-04-27 15:15:12 +0900

    :recycle: 만료시간 반환 형태 수정

--- src/main/java/com/select/choice/domain/auth/presentation/data/response/TokenResponse.java
+++ src/main/java/com/select/choice/domain/auth/presentation/data/response/TokenResponse.java
@@ -1,5 +1,6 @@
 package com.select.choice.domain.auth.presentation.data.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,8 @@ import java.time.LocalDateTime;
 public class TokenResponse {
     private final String accessToken;
     private final String refreshToken;
+    @JsonFormat(pattern = "yyyy-MM-dd 'T' HH:mm:ss")
     private final LocalDateTime accessExpiredTime;
+    @JsonFormat(pattern = "yyyy-MM-dd 'T' HH:mm:ss")
     private final LocalDateTime refreshExpiredTime;
 }
