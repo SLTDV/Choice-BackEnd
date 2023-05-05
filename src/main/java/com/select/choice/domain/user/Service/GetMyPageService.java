bd3a542012b5773d7fb714d1b337b13c83b00819
shgur7236 <shgurtns7236@naver.com>
2023-05-06 01:53:32 +0900

    :recycle: SRP를 준수하여 코드 변경

---
+++ src/main/java/com/select/choice/domain/user/Service/GetMyPageService.java
@@ -0,0 +1,7 @@
+package com.select.choice.domain.user.Service;
+
+import com.select.choice.domain.user.presentation.data.dto.MyPageDto;
+
+public interface GetMyPageService {
+    MyPageDto getMyPage();
+}
---
+++ src/main/java/com/select/choice/domain/user/Service/GetWebMyPageService.java
@@ -0,0 +1,7 @@
+package com.select.choice.domain.user.Service;
+
+import com.select.choice.domain.user.presentation.data.dto.WebMyPageDto;
+
+public interface GetWebMyPageService {
+    WebMyPageDto getMyPage();
+}
