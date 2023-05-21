42675aaa239d9b50db757cab8bcf0badc19d7621
shgur7236 <shgurtns7236@naver.com>
2023-05-22 08:52:42 +0900

    :bug: size max 값을 10으로 수정

--- src/main/java/com/select/choice/domain/auth/presentation/data/request/SignUpRequest.java
+++ src/main/java/com/select/choice/domain/auth/presentation/data/request/SignUpRequest.java
@@ -12,7 +12,7 @@ public class SignUpRequest {
     private final String phoneNumber;
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private final String password;
-    @Size(min = 2, max = 6, message = "닉네임은 2글자 이상 6글자 이하입니다.")
+    @Size(min = 2, max = 10, message = "닉네임은 2글자 이상 10글자 이하입니다.")
     private final String nickname;
     private final Optional<String> profileImgUrl;
 }
