d34416d7a76af5bf1f27afdd4babc1e049392be3
shgur7236 <shgurtns7236@naver.com>
2023-05-25 21:01:12 +0900

    :bug: @Size 추가

--- src/main/java/com/select/choice/domain/user/presentation/data/request/ChangeProfileImageRequest.java
+++ src/main/java/com/select/choice/domain/user/presentation/data/request/ChangeProfileImageRequest.java
@@ -2,10 +2,13 @@ package com.select.choice.domain.user.presentation.data.request;
 
 import lombok.*;
 
+import javax.validation.constraints.Size;
+
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChangeProfileImageRequest {
+    @Size(min = 2, max = 10, message = "닉네임은 2글자 이상 10글자 이하입니다.")
     private String image;
 }
