7028a04c422fe8d81a4a1d55335b97ebdf68f467
shgur7236 <shgurtns7236@naver.com>
2023-05-25 21:06:10 +0900

    :bug: 실수로 프로필이미지에 닉네임 제한값 적용했음

--- src/main/java/com/select/choice/domain/user/presentation/data/request/ChangeNicknameRequest.java
+++ src/main/java/com/select/choice/domain/user/presentation/data/request/ChangeNicknameRequest.java
@@ -5,10 +5,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChangeNicknameRequest {
+    @Size(min = 2, max = 10, message = "닉네임은 2글자 이상 10글자 이하입니다.")
     private String nickname;
 }
--- src/main/java/com/select/choice/domain/user/presentation/data/request/ChangeProfileImageRequest.java
+++ src/main/java/com/select/choice/domain/user/presentation/data/request/ChangeProfileImageRequest.java
@@ -9,6 +9,5 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChangeProfileImageRequest {
-    @Size(min = 2, max = 10, message = "닉네임은 2글자 이상 10글자 이하입니다.")
     private String image;
 }
