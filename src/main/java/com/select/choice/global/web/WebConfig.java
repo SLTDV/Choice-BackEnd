48d3346bf06c8d1110a3ef75e0f74aaa5d46bb97
shgur7236 <shgurtns7236@naver.com>
2023-06-14 17:17:24 +0900

    :sparkles: origin 추가

--- src/main/java/com/select/choice/global/web/WebConfig.java
+++ src/main/java/com/select/choice/global/web/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override //CORS 설정
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://server.choice-time.com", "https://choice-hazel.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://www.choice-time.com", "https://choice-hazel.vercel.app", "https://choice-time.com")
                 .allowedMethods("GET","POST","DELETE","PUT","OPTIONS","PATCH")
                 .allowCredentials(true)
                 .maxAge(3600);
