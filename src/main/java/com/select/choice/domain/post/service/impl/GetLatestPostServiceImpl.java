2f86fd2ad8f96f55f7fad9184bf2c157eaafdcae
shgur7236 <shgurtns7236@naver.com>
2023-06-19 13:56:38 +0900

    :sparkles: 로직 변경

--- src/main/java/com/select/choice/domain/post/service/impl/GetLatestPostServiceImpl.java
+++ src/main/java/com/select/choice/domain/post/service/impl/GetLatestPostServiceImpl.java
@@ -7,6 +7,9 @@ import com.select.choice.domain.post.presentation.data.dto.TotalPageAndWebPostDt
 import com.select.choice.domain.post.presentation.data.dto.WebPostDto;
 import com.select.choice.domain.post.service.GetLatestPostsService;
 import com.select.choice.domain.post.util.PostConverter;
+import com.select.choice.domain.user.domain.entity.User;
+import com.select.choice.domain.user.domain.repository.BlockedUserRepository;
+import com.select.choice.domain.user.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,19 +17,34 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class GetLatestPostServiceImpl implements GetLatestPostsService {
     private final PostRepository postRepository;
     private final PostConverter postConverter;
+    private final UserUtil userUtil;
+    private final BlockedUserRepository blockedUserRepository;
 
     @Override
     public List<PostDto> getLatestPost(Pageable pageable) {
+        User currentUser = userUtil.currentUser();
         List<Post> list = postRepository.findAll(
                 PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("idx").descending()))
                 .toList();
-        return postConverter.toDto(list);
+
+        List<Post> filteredList = list.stream()
+                .filter(post -> {
+                    boolean isBlockedByCurrentUser = currentUser.getBlockedUsers().stream()
+                            .anyMatch(blockedUser -> blockedUser.getBlockedUser().equals(post.getUser()));
+                    boolean isBlockedByOtherUser = currentUser.getBlockingUsers().stream()
+                            .anyMatch(blockedUser -> blockedUser.getBlockingUser().equals(post.getUser()));
+                    return !isBlockedByCurrentUser && !isBlockedByOtherUser;
+                })
+                .collect(Collectors.toList());
+
+        return postConverter.toDto(filteredList);
     }
 
     @Override
