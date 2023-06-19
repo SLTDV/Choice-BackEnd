9700530a74b02f60670888e1ed11a7d58aaee82e
shgur7236 <shgurtns7236@naver.com>
2023-06-19 09:58:23 +0900

    :sparkles: 유저 차단 테아블 생성

---
+++ src/main/java/com/select/choice/domain/user/domain/entity/Block.java
@@ -0,0 +1,32 @@
+package com.select.choice.domain.user.domain.entity;
+
+import com.select.choice.global.common.entity.BaseIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Block extends BaseIdEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User blockingUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User blockedUser;
+
+    @Builder
+    public Block(User blockingUser, User blockedUser) {
+        this.blockingUser = blockingUser;
+        this.blockedUser = blockedUser;
+    }
+}
---
+++ src/main/java/com/select/choice/domain/user/domain/repository/BlockRepository.java
@@ -0,0 +1,7 @@
+package com.select.choice.domain.user.domain.repository;
+
+import com.select.choice.domain.user.domain.entity.Block;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockRepository extends JpaRepository<Block, Long> {
+}
