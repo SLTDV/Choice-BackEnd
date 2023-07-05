f721fb251bb73549f2ff79cf8eb48ca196a527df
shgur7236 <shgurtns7236@naver.com>
2023-07-05 20:13:48 +0900

    :sparkles: 예외처리

--- src/main/java/com/select/choice/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/select/choice/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -4,6 +4,8 @@ package com.select.choice.domain.auth.domain.repository;
 import com.select.choice.domain.auth.domain.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    RefreshToken findByRefreshToken(String refreshToken);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
