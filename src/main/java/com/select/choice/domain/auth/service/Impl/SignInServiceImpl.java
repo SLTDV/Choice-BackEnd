04f84edbc96074a6b768f61e92dc8fc35d18dc31
shgur7236 <shgurtns7236@naver.com>
2023-09-04 11:53:56 +0900

    :white_check_mark: test code

--- src/main/java/com/select/choice/domain/auth/service/Impl/SignInServiceImpl.java
+++ src/main/java/com/select/choice/domain/auth/service/Impl/SignInServiceImpl.java
@@ -26,7 +26,7 @@ public class SignInServiceImpl implements SignInService {
     @Transactional(rollbackFor = Exception.class)
     public TokenDto signIn(SignInDto signInDto) {
         User user = userUtil.findUserByPhoneNumber(signInDto.getPhoneNumber());
-        System.out.println(signInDto.getDeviceToken());
+        System.out.println(signInDto.getDeviceToken() + "||");
         if(signInDto.getDeviceToken().isPresent()){
             user.updateFCMToken(signInDto.getDeviceToken().toString());
         }
